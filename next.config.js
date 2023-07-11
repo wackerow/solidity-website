bc45bf7eeb93a05752eca29a0e9b48fd97a5c2e8
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-07-11 10:03:21 -0700

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
   experimental: { esmExternals: true },
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
+  basePath: '/solidity-website',
+  assetPrefix: '/solidity-website/',
 }
 
 const removeImports = require('next-remove-imports')()
