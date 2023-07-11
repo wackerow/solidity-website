9b7227402ef2c1143e47dff08f4ff60ec4fc57f6
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-07-11 09:26:24 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
   experimental: { esmExternals: true },
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
 }
 
 const removeImports = require('next-remove-imports')()
